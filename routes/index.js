d9c4687b39c3caf2607f9bea25da8cc9d85694b2
sirawit.mahanin <siraiwt.mn@gmail.com>
2022-08-26 22:34:57 +0700

    Add CSR header

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,16 @@ var comments = ['Hack me if you can'];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // Disallows any kind of in-line scripts / scripts that don't come from self (our own domain)
+  // res.setHeader('Content-Security-Policy', "script-src 'self'");
+
+  // To allow scripts from trusted source
+  // res.setHeader('Content-Security-Policy', "script-src 'self' https://fonts.googleapis.com");
+
+  // Disallows in-line styles
+  // but this also block our inline styles!
+  res.setHeader('Content-Security-Policy', "script-src 'self' https://fonts.googleapis.com; style-src 'self'");
+
   res.render('index', { title: 'Content Security Policy', comments: comments });
 });
 
